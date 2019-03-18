ef2076a5fa2ee5b727891b372b5aa2f54b4a12fc
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-18 13:56:50 +0100

    2019-03-18

--- Assets/_GameAssets/Scripts/MrHardcode.cs
+++ Assets/_GameAssets/Scripts/MrHardcode.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MrHardcode : MonoBehaviour
 {
+    public enum State { Idle, Walking, Running, Jumping }
+    public State estado = State.Idle;
+    float x;
     float z;
     Animator animator;
+
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -13,13 +18,90 @@ public class MrHardcode : MonoBehaviour
     void Update()
     {
         z = Input.GetAxis("Vertical");
+        x = Input.GetAxis("Horizontal");
+        switch (estado)
+        {
+            case State.Idle:
+                ManageIdle();
+                break;
+            case State.Walking:
+                ManageWalking();
+                break;
+            case State.Running:
+                ManageRunning();
+                break;
+        }
+        Rotate();
+    }
+
+    private void ManageRunning()
+    {
+        if (z <= 0)
+        {
+            StopMotion();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            StopRunning();
+        }
+    }
+
+    private void ManageWalking()
+    {
+        if (z <= 0)
+        {
+            StopMotion();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartRunning();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartJumping();
+        }
+    }
+
+    private void ManageIdle()
+    {
         if (z > 0)
         {
-            animator.SetBool("Walking", true);
-        } else
-        {
-            animator.SetBool("Walking", false);
+            StartWalking();
         }
+    }
 
+    private void StopRunning()
+    {
+        animator.SetBool("Running", false);
+        estado = State.Walking;
+    }
+
+    private void StartRunning()
+    {
+        animator.SetBool("Running", true);
+        estado = State.Running;
+    }
+
+    private void StopMotion()
+    {
+        animator.SetBool("Running", false);
+        animator.SetBool("Walking", false);
+        estado = State.Idle;
+    }
+
+    private void StartWalking()
+    {
+        animator.SetBool("Walking", true);
+        estado = State.Walking;
+    }
+
+    private void StartJumping()
+    {
+        animator.SetTrigger("Jumping");
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(new Vector3(0, x, 0));
     }
 }

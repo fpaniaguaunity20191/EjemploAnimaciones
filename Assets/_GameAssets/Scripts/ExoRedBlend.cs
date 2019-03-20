abacc685acd6386b61f7dd57eddfc63ccc161c7b
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-20 12:52:51 +0100

    2019-03-20

---
+++ Assets/_GameAssets/Scripts/ExoRedBlend.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExoRedBlend : MonoBehaviour
+{
+    float speed = 0;
+    Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            speed += 0.05f;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            speed -= 0.05f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Shooting");
+        }
+        speed = Mathf.Min(1, speed);
+        speed = Mathf.Max(0, speed);
+        animator.SetFloat("Speed", speed);
+    }
+}

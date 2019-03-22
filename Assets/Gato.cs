0b71623e3efb00fe5ce6c245317a96d83e838b14
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-22 13:31:17 +0100

    2019-03-22

---
+++ Assets/Gato.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Gato : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOMoveX(5, 5).SetLoops(-1,LoopType.Yoyo);
+        GetComponent<Renderer>().material.DOColor(Color.red, 5).SetLoops(-1);
+        GetComponent<Renderer>().material.DOFade(0, 3).SetLoops(2).OnComplete(Destruir);
+    }
+    void Destruir()
+    {
+        //Destroy(this.gameObject);
+    }
+}
